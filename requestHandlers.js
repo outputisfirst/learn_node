223ea49354b74f405441d493ee87ea77c07f54c9
outputisfirst <outputisfirst@gmail.com>
2021-01-07 22:50:54 +0900

    post kinou jissou

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,30 +1,31 @@
-let exec = require('child_process').exec;
+let querystring = require("querystring");
 
-function start(response) {
+function start(response, postData) {
   console.log('Request handler "start" was called');
 
-  exec('find /', 
-  { timeout: 10000, maxBuffer: 20000*1024 },
-  function (error, stdout, stderr) {
-    response.writeHead(200, {"Content-type": "text/plain"});
-    response.write(stdout);
-    response.end();
-  });
+  let body = '<html>' +
+    '<head>' +
+    '<meta http-equiv="Content-Type" content="text/html; '+
+    'charset=UTF-8" />' +
+    '</head>'+
+    '<body>' +
+    '<form action="/upload" method="post">' +
+    '<textarea name="text" rows="20" cols="60"></textarea>' +
+    '<input type="submit" value="Submit text" />' +
+    '</form>' +
+    '</body>' +
+    '</html>';
 
-/*
-  function sleep(milliSeconds) {
-    let startTime = new Date().getTime();
-    while (new Date().getTime() < startTime + milliSeconds);
-  }
-  // sleep(10000);
-*/
-
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write(body);
+  response.end();
 }
 
-function upload(response) {
+function upload(response, postData) {
   console.log('Request handler "upload" was called');
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello Upload");
+  response.write("You've sent: " + 
+  querystring.parse(postData).text );
   response.end()
 }
 
