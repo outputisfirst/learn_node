5ab16d7be1215af2f86c0f45a04d02167908aab7
outputisfirst <outputisfirst@gmail.com>
2021-01-07 22:12:41 +0900

    non blocking

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,19 +1,31 @@
 let exec = require('child_process').exec;
 
-function start() {
+function start(response) {
   console.log('Request handler "start" was called');
-  let content = 'empty';
 
-  exec('ls -lah', function (error, stdout, stderr) {
-    content = stdout;
+  exec('find /', 
+  { timeout: 10000, maxBuffer: 20000*1024 },
+  function (error, stdout, stderr) {
+    response.writeHead(200, {"Content-type": "text/plain"});
+    response.write(stdout);
+    response.end();
   });
 
-  return content;
+/*
+  function sleep(milliSeconds) {
+    let startTime = new Date().getTime();
+    while (new Date().getTime() < startTime + milliSeconds);
+  }
+  // sleep(10000);
+*/
+
 }
 
-function upload() {
+function upload(response) {
   console.log('Request handler "upload" was called');
-  return 'Hello Upload';
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello Upload");
+  response.end()
 }
 
 exports.start = start;
